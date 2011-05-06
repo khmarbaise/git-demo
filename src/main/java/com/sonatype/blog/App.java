3e601c9fc16233d05a6f15f1906e5f496b099450
Karl Heinz Marbaise <kama@soebes.de>
2011-05-06 15:51:01 +0200

    - Start

---
+++ src/main/java/com/sonatype/blog/App.java
@@ -0,0 +1,13 @@
+package com.sonatype.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
